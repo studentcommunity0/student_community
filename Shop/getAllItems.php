a6a72f7d016e9093d64e1a3c9d1186e10d9cbdad
mohammadalsukaibi <46539624+mohammadalsukaibi@users.noreply.github.com>
2020-07-15 15:41:28 +0300

    Add files via upload

---
+++ Shop/getAllItems.php
@@ -0,0 +1,78 @@
+<?php
+    session_start();
+    $userID = $_SESSION["id"];
+
+    $connection=mysqli_connect("localhost","root","","student_community");
+    if(mysqli_connect_errno()){
+        die("ERROR: could not connect" . mysqli_connect_error());
+    }
+
+    $sql = "SELECT * FROM item";
+    if($result=mysqli_query($connection,$sql)){
+        if(mysqli_num_rows($result)>0){
+            echo "  <div class='row' style='margin-bottom:1%'>";
+            echo "      <div class='col-4'></div>";
+            echo "          <div class='col-8' style='padding-left:6%'>";
+            echo "              <div class='input-group-append'>";
+            echo "                  <button class='btn black-text-orange-bg' style='border-radius: 5%;font-weight:bold;margin-right:2%' type='button' onclick='getTheItems()'>";
+            echo "                      All items";
+            echo "                  </button>";
+            echo "                  <button class='btn black-text-orange-bg' style='border-radius: 5%;font-weight:bold;margin-right:2%' type='button' onclick='myItems()'>";
+            echo "                      My items";
+            echo "                  </button>";
+            echo "                <button class='btn black-text-orange-bg' style='border-radius: 5%; font-weight:bold' type='button' onclick='addItemForm()'>";
+            echo "                      + add new item";
+            echo "                </button>";
+            echo "            </div>";
+            echo "        </div>";
+            echo "    </div>";
+            echo "<div class='row' style='background-color:#3d3d3d; color:orange; font-weight:bold;height:15%'>";
+            echo "  <div class='col'>Item Name</div>";
+            echo "  <div class='col'>Description</div>";
+            echo "  <div class='col'>Price</div>";
+            echo "  <div class='col'>Contact Information</div>";
+            echo "  <div class='col'></div>";
+            echo "</div>";
+            while($row=mysqli_fetch_array($result)){
+                echo "<div class='row' class='items' style='background-color:#dddada; margin-bottom:1%;box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2); height:20%;padding:1%'>";
+                echo "  <div class='col'>";
+                echo "      <a class='item-name'>" . $row['item'] . "</a>";
+                echo "  </div>";
+                echo "  <div class='col'>";
+                echo "      <a>" . $row['description'] . "</a>";
+                echo "  </div>";
+                echo "  <div class='col item-name'>";
+                echo "      <a>" . $row['price'] . "SR</a>";
+                echo "  </div>";
+                if($row['status']=='sold'){
+                    echo "  <div class='col'>";
+                    echo "      <a class='sold' style='color: rgb(199, 30, 30)'>" . $row['status'] . "</a>";
+                    echo "  </div>";
+                }
+                else{
+                    echo "  <div class='col contact-info'>";
+                    echo "      <a>" . $row['contact'] . "</a>";
+                    echo "  </div>";
+                }
+                echo "<div class='col'>";
+                if($userID == $row['userid']){
+                    echo "  <select class='form-control' style='background-color: #3d3d3d; color:orange; font-weight:bold' onchange='changeStatus(this.value," . $row['id'] . ")'>";
+                    if($row['status']=='not sold'){
+                        echo "      <option>not sold</option>";
+                        echo "      <option>sold</option>";
+                    }
+                    else{
+                        echo "      <option>sold</option>";
+                        echo "      <option>not sold</option>";
+                    }
+                    echo "  </select>";
+                }
+                echo "</div>";
+                echo "</div>";
+            }
+        }
+    }
+    else{
+        echo "Failed to insert the item";
+    }
+?>
---
+++ university/university.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Universities</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="universityStyling.css">
+        <link rel="stylesheet" href="../assets/css/maintheme.css">
+        <link rel="stylesheet" href="../assets/css/sidenavbar.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="shopScripting.js"></script>
+        <script src="../assets/js/sidenavbar.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    </head>
+    <body>
+        <!--NAVBAR-->
+        <?php require('../studentcommunitynavbar.php');?>
+
+        <div class="container-fluid">
+
+            <h1 id="title">All the Universities in Saudi Arabia</h1>
+
+
+            <div>
+                
+                <?php include("getAllUniversities.php");  ?>
+
+                
+                
+            </div>
+
+        </div>
+        
+    </body>
+</html>
